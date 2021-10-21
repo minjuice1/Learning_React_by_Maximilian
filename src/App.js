50fab1962e2ae00de47066801f23b93eff69060e
minjuice1 <minjuice93@gmail.com>
2021-10-21 16:04:37 +0900

    새로운 Expense 구성요소 만들고 css 추가

--- src/App.js
+++ src/App.js
@@ -1,6 +1,26 @@
 import ExpenseItem from "./components/ExpenseItem";
 
 function App() {
+	const NewExpenses = [
+		{
+			id: "n1",
+			title: "Shower Towel",
+			amount: 50.12,
+			date: new Date(2021, 3, 14),
+		},
+		{
+			id: "n2",
+			title: "A Trip to Jeju",
+			amount: 1400.9,
+			date: new Date(2021, 8, 7),
+		},
+		{
+			id: "n3",
+			title: "New Computer",
+			amount: 2094.52,
+			date: new Date(2021, 12, 4),
+		},
+	];
 	const expenses = [
 		{
 			id: "e1",
@@ -26,6 +46,23 @@ function App() {
 	return (
 		<div>
 			<h2>Let's get started!</h2>
+			<div className="new-expenses">
+				<ExpenseItem
+					title={NewExpenses[0].title}
+					amount={NewExpenses[0].amount}
+					date={NewExpenses[0].date}
+				/>
+				<ExpenseItem
+					title={NewExpenses[1].title}
+					amount={NewExpenses[1].amount}
+					date={NewExpenses[1].date}
+				/>
+				<ExpenseItem
+					title={NewExpenses[2].title}
+					amount={NewExpenses[2].amount}
+					date={NewExpenses[2].date}
+				/>
+			</div>
 			<ExpenseItem
 				title={expenses[0].title}
 				amount={expenses[0].amount}
--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,5 +1,6 @@
 import ExpenseDate from "./ExpenseDate";
 import "./ExpenseItem.css";
+import "./NewExpenseItem.css";
 
 function ExpenseItem(props) {
 	return (
