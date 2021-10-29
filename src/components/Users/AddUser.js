4e01bdf18e627aec99b98b9a60ee5c5d6543cf47
minjuice1 <minjuice93@gmail.com>
2021-10-29 20:25:22 +0900

    ErrorModal 렌더링 / error일시에만 나타나게 수정 예정

--- src/components/Users/AddUser.js
+++ src/components/Users/AddUser.js
@@ -2,6 +2,7 @@ import React, { useState } from "react";
 import Button from "../UI/Button";
 import Card from "../UI/Card";
 import classes from "./Adduser.module.css";
+import ErrorModal from "../UI/ErrorModal";
 
 const AddUser = (props) => {
 	const [enteredUsername, setEnteredUsername] = useState("");
@@ -29,25 +30,28 @@ const AddUser = (props) => {
 	};
 
 	return (
-		<Card className={classes.input}>
-			<form onSubmit={addUserHandler}>
-				<label htmlFor="username">Username</label>
-				<input
-					id="username"
-					type="text"
-					value={enteredUsername}
-					onChange={usernameChangeHandler}
-				></input>
-				<label htmlFor="age">Age (Years)</label>
-				<input
-					id="age"
-					type="number"
-					value={enteredAge}
-					onChange={ageChangeHandler}
-				></input>
-				<Button type="submit">Add User</Button>
-			</form>
-		</Card>
+		<div>
+			<ErrorModal title="An error occured!" message="Something went wrong!" />
+			<Card className={classes.input}>
+				<form onSubmit={addUserHandler}>
+					<label htmlFor="username">Username</label>
+					<input
+						id="username"
+						type="text"
+						value={enteredUsername}
+						onChange={usernameChangeHandler}
+					></input>
+					<label htmlFor="age">Age (Years)</label>
+					<input
+						id="age"
+						type="number"
+						value={enteredAge}
+						onChange={ageChangeHandler}
+					></input>
+					<Button type="submit">Add User</Button>
+				</form>
+			</Card>
+		</div>
 	);
 };
 
