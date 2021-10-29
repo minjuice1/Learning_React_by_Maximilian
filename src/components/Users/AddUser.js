682da7fd3e2273e9a35b8251b4ded76cd995c800
minjuice1 <minjuice93@gmail.com>
2021-10-29 14:51:56 +0900

    onclick, change 핸들러로 username, age 받아옴

--- src/components/Users/AddUser.js
+++ src/components/Users/AddUser.js
@@ -1,20 +1,37 @@
-import React from "react";
+import React, { useState } from "react";
+import Button from "../UI/Button";
 import Card from "../UI/Card";
 import classes from "./Adduser.module.css";
 
-const AddUser = () => {
+const AddUser = (props) => {
+	const [enteredUsername, setEnteredUsername] = useState("");
+	const [enteredAge, setEnteredAge] = useState("");
+
 	const addUserHandler = (event) => {
 		event.preventDefault();
+		console.log(enteredUsername, enteredAge);
+	};
+
+	const usernameChangeHandler = (event) => {
+		setEnteredUsername(event.target.value);
+	};
+
+	const ageChangeHandler = (event) => {
+		setEnteredAge(event.target.value);
 	};
 
 	return (
 		<Card className={classes.input}>
 			<form onSubmit={addUserHandler}>
 				<label htmlFor="username">Username</label>
-				<input id="username" type="text"></input>
+				<input
+					id="username"
+					type="text"
+					onChange={usernameChangeHandler}
+				></input>
 				<label htmlFor="age">Age (Years)</label>
-				<input id="age" type="number"></input>
-				<button type="submit">Add User</button>
+				<input id="age" type="number" onChange={ageChangeHandler}></input>
+				<Button type="submit">Add User</Button>
 			</form>
 		</Card>
 	);
