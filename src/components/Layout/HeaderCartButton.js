54b2790942c7a97dc08c0ff5558f30a5d74675aa
minjuice1 <minjuice93@gmail.com>
2021-11-02 17:35:14 +0900

    header button 부분 CSS 오류 수정

--- src/components/Layout/HeaderCartButton.js
+++ src/components/Layout/HeaderCartButton.js
@@ -3,12 +3,12 @@ import classes from "./HeaderCartButton.module.css";
 
 const HeaderCartButton = (props) => {
 	return (
-		<button classes={classes.button}>
-			<span classes={classes.icon}>
+		<button className={classes.button}>
+			<span className={classes.icon}>
 				<CartIcon />
 			</span>
 			<span>Your Cart</span>
-			<span classes={classes.badge}>3</span>
+			<span className={classes.badge}>3</span>
 		</button>
 	);
 };
